ed93e6c3a05ef033ff9299afb81c1a7363e4d1fd
Riverprawn <greensaltfish@hotmail.com>
2023-04-10 09:01:09 +0800

    feat: removed unused data types.

--- contracts/libraries/DataTypes.sol
+++ contracts/libraries/DataTypes.sol
@@ -5,30 +5,4 @@ uint256 constant STRIKE_PRICE_GAP_LIST_SIZE = 6;
 uint256 constant DURATION_LIST_SIZE = 4;
 
 library DataTypes {
-    struct OptionData {
-        uint8 optionType; // 0 - call, 1 - put, 2 - queued call, 3 - queued put
-        uint8 strikePriceGapIndex;
-        uint8 durationIndex;
-        address collection;
-        uint256 amount;
-        uint256 expiration;
-        uint256 openPrice;
-        uint256 premium;
-    }
-
-    struct CollectionConfiguration {
-        bool paused;
-        bool activated;
-        uint16 weight; // percentage: 10000 means 100%
-        address callToken;
-        address putToken;
-        address premium;
-    }
-
-    struct CollectionData {
-        CollectionConfiguration config;
-        int256 delta;
-        int256 unrealizedPNL;
-    }
-
 }
