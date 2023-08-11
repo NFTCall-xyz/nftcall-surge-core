080a6636f4c6a7103757678e7e888bc9c8c54dc9
gordon199404 <gordon199404@outlook.com>
2023-08-11 10:08:59 +0800

    feat: MintableERC20 add Ownable,maxMintAmount

--- contracts/mocked/MintableERC20.sol
+++ contracts/mocked/MintableERC20.sol
@@ -1,27 +1,72 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity ^0.8.4;
 
-import {ERC20} from '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @title ERC20Mintable
- * @dev ERC20 minting logic
- */
-contract MintableERC20 is ERC20 {
-  constructor(
-    string memory name,
-    string memory symbol
-  ) ERC20(name, symbol) {
+contract MintableERC20 is ERC20, Ownable {
+    mapping(address => bool) private _whitelistedAddresses;
+    mapping(address => uint256) private _mintedAmounts;
+    uint256 private _maxMintAmountPerUser;
 
-  }
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialMaxMintAmountPerUser
+    ) ERC20(name, symbol) {
+        _maxMintAmountPerUser = initialMaxMintAmountPerUser;
+    }
 
-  /**
-   * @dev Function to mint tokens
-   * @param value The amount of tokens to mint.
-   * @return A boolean that indicates if the operation was successful.
-   */
-  function mint(uint256 value) public returns (bool) {
-    _mint(_msgSender(), value);
-    return true;
-  }
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        super._beforeTokenTransfer(from, to, amount);
+
+        if (from != address(0)) {
+            require(
+                _whitelistedAddresses[to] || _whitelistedAddresses[from],
+                "Recipient is not whitelisted"
+            );
+        }
+    }
+
+    function setWhitelistAddress(
+        address account,
+        bool whitelisted
+    ) external onlyOwner {
+        _whitelistedAddresses[account] = whitelisted;
+    }
+
+    function setMaxMintAmountPerUser(uint256 maxMintAmount) external onlyOwner {
+        _maxMintAmountPerUser = maxMintAmount;
+    }
+
+    function mint() public returns (bool) {
+        require(
+            _mintedAmounts[_msgSender()] < _maxMintAmountPerUser,
+            "You have already minted the maximum amount allowed."
+        );
+
+        uint256 mintAmount = _maxMintAmountPerUser -
+            _mintedAmounts[_msgSender()];
+
+        if (mintAmount > 0) {
+            _mintedAmounts[_msgSender()] += mintAmount;
+            _mint(_msgSender(), mintAmount);
+            return true;
+        }
+
+        return false;
+    }
+
+    function ownerMint(
+        address to,
+        uint256 mintAmount
+    ) public onlyOwner returns (bool) {
+        _mintedAmounts[to] += mintAmount;
+        _mint(to, mintAmount);
+        return true;
+    }
 }
