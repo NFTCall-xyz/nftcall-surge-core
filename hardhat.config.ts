87a919eefbc3d30f3bb5665caf6c134e7640aa95
gordon199404 <gordon199404@outlook.com>
2024-02-11 20:12:50 +0800

    feat: 调整 hardhat 配置项

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,31 @@
 import { HardhatUserConfig } from "hardhat/config";
-import path from 'path';
-import fs from 'fs';
+import path from "path";
+import fs from "fs";
 import "@nomicfoundation/hardhat-toolbox";
-require("@nomicfoundation/hardhat-foundry");
+// require("@nomicfoundation/hardhat-foundry");
 import "dotenv/config";
 
 const SKIP_LOAD = process.env.SKIP_LOAD === "true";
 
-if ( !SKIP_LOAD ) {
-    const tasksPath = path.join(__dirname, "tasks");
-    if( fs.existsSync(tasksPath) ){
-        fs.readdirSync(tasksPath)
-        .filter((pth) => pth.includes(".ts"))
-        .forEach((task) => {
-            require(`${tasksPath}/${task}`);
-        }
-    );
+if (!SKIP_LOAD) {
+  const tasksPath = path.join(__dirname, "tasks");
+  if (fs.existsSync(tasksPath)) {
+    fs.readdirSync(tasksPath)
+      .filter((pth) => pth.includes(".ts"))
+      .forEach((task) => {
+        require(`${tasksPath}/${task}`);
+      });
   }
-};
+}
 
-const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || '';
-const INFURA_KEY = process.env.INFURA_KEY || '';
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || "";
+const INFURA_KEY = process.env.INFURA_KEY || "";
 
 const config: HardhatUserConfig = {
   typechain: {
     outDir: "types",
     target: "ethers-v5",
-    discriminateTypes: true,
+    discriminateTypes: false,
   },
   solidity: {
     version: "0.8.17",
@@ -46,7 +45,9 @@ const config: HardhatUserConfig = {
     },*/
     arbitrumGoerli: {
       chainId: 421613,
-      url: INFURA_KEY ? `https://goerli.infura.io/v3/${INFURA_KEY}` : `https://arb-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      url: INFURA_KEY
+        ? `https://goerli.infura.io/v3/${INFURA_KEY}`
+        : `https://arb-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       // accounts: [process.env.ARB_GOERLI_PRIVATE_KEY || '', process.env.OPERATOR_PRIVATE_KEY || ''],
     },
     blastSepolia: {
@@ -54,7 +55,7 @@ const config: HardhatUserConfig = {
       url: "https://sepolia.blast.io",
       accounts: [process.env.OWNER_PRIVATE_KEY as string, process.env.OPERATOR_PRIVATE_KEY as string],
       gasPrice: 1000000000,
-    }
+    },
   },
   gasReporter: {
     enabled: true,
@@ -62,8 +63,8 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: {
-      arbitrumGoerli: process.env.ARB_SCAN_KEY || '',
-    }
+      arbitrumGoerli: process.env.ARB_SCAN_KEY || "",
+    },
   },
 };
 
